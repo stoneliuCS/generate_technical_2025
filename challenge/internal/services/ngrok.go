ab5c600fd73ba1fff926024ca5e1dd77660da863
Zachary Croft <croft.z@northeastern.edu>
2025-07-26 11:43:08 -0400

    refactored ngrok signatures

--- challenge/internal/services/ngrok.go
+++ challenge/internal/services/ngrok.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http"
+	"strings"
 )
 
 type NgrokChallenge struct {
@@ -10,7 +12,9 @@ type NgrokChallenge struct {
 }
 
 type NgrokChallengeScore struct {
-	Score int
+	Valid  bool
+	Score  int
+	Reason string // optional, only set when Valid = false
 }
 
 type NgrokRequest interface {
@@ -28,8 +32,7 @@ type NgrokGetRequest struct {
 	Name           string
 	Points         int
 	Path           string
-	ExpectedCount  int
-	ExpectedFilter func(DetailedAlien) bool
+	ExpectedAliens []DetailedAlien
 }
 
 type Stats struct {
@@ -45,5 +48,78 @@ func (t NgrokGetRequest) Execute(client *http.Client, baseURL string) error {
 }
 
 func generateRandomFilterTests(rng *rand.Rand, aliens []DetailedAlien) []NgrokRequest {
-	panic("Not implemented.")
+	requests := []NgrokRequest{}
+	// Filter by type:
+	randomType := alienTypes[rng.Intn(len(alienTypes))]
+
+	var queryParamsType []string
+	queryParamsType = append(queryParamsType, fmt.Sprintf("type=%s", randomType))
+
+	expectedAliensType := filterAliensByType(aliens, randomType)
+
+	request := NgrokGetRequest{
+		Name:           fmt.Sprintf("Filter by type=%s", randomType),
+		Points:         NGROK_FILTER_TYPE_POINTS,
+		Path:           "/api/aliens?" + strings.Join(queryParamsType, "&"),
+		ExpectedAliens: expectedAliensType,
+	}
+
+	requests = append(requests, request)
+
+	// Filter by SPD
+	minSPD := rng.Intn(ALIEN_ATK_HP_SPD_UPPER + 1)
+
+	var queryParamsSPD []string
+	queryParamsSPD = append(queryParamsSPD, fmt.Sprintf("spd_gte=%s", randomType))
+
+	expectedAliensSPD := filterAliensByMinSPD(aliens, minSPD)
+
+	request = NgrokGetRequest{
+		Name:           fmt.Sprintf("Filter by spd>=%s", randomType),
+		Points:         NGROK_FILTER_SPD_POINTS,
+		Path:           "/api/aliens?" + strings.Join(queryParamsSPD, "&"),
+		ExpectedAliens: expectedAliensSPD,
+	}
+
+	requests = append(requests, request)
+
+	var queryParamsATK []string
+	queryParamsATK = append(queryParamsATK, fmt.Sprintf("atk_gte=%s", 3))
+	queryParamsATK = append(queryParamsATK, fmt.Sprintf("atk_lt=%s", 2))
+
+	// The expected aliens is an empty array--the filters contradict each other.
+	expectedAliensATK := []DetailedAlien{}
+
+	request = NgrokGetRequest{
+		Name:           "Filter by atk contradict",
+		Points:         NGROK_FILTER_SPD_POINTS,
+		Path:           "/api/aliens?" + strings.Join(queryParamsSPD, "&"),
+		ExpectedAliens: expectedAliensATK,
+	}
+
+	requests = append(requests, request)
+
+	return requests
+}
+
+// Filter Helpers
+
+func filterAliensByType(aliens []DetailedAlien, alienType AlienType) []DetailedAlien {
+	var filtered []DetailedAlien
+	for _, alien := range aliens {
+		if alien.Type == alienType {
+			filtered = append(filtered, alien)
+		}
+	}
+	return filtered
+}
+
+func filterAliensByMinSPD(aliens []DetailedAlien, minSPD int) []DetailedAlien {
+	var filtered []DetailedAlien
+	for _, alien := range aliens {
+		if alien.Spd >= minSPD {
+			filtered = append(filtered, alien)
+		}
+	}
+	return filtered
 }
