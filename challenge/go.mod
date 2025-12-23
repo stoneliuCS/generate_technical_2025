83e85feab60d9af508584fdf1f01e072fd6a45b1
Zachary Croft <croft.z@northeastern.edu>
2025-12-22 19:16:08 -0500

    fix: ngrok tests no longer fail due to dependent global variables

--- challenge/go.mod
+++ challenge/go.mod
@@ -4,32 +4,34 @@ go 1.24.4
 
 require (
 	github.com/MarceloPetrucio/go-scalar-api-reference v0.0.0-20240521013641-ce5d2efe0e06
+	github.com/a-h/templ v0.3.920
 	github.com/docker/go-connections v0.5.0
 	github.com/go-faster/errors v0.7.1
-	github.com/go-faster/jx v1.1.0
+	github.com/go-faster/jx v1.2.0
 	github.com/google/uuid v1.6.0
-	github.com/ogen-go/ogen v1.14.0
+	github.com/ogen-go/ogen v1.18.0
 	github.com/orandin/slog-gorm v1.4.0
 	github.com/sethvargo/go-envconfig v1.3.0
-	github.com/stretchr/testify v1.10.0
+	github.com/stretchr/testify v1.11.1
 	github.com/testcontainers/testcontainers-go v0.37.0
 	github.com/testcontainers/testcontainers-go/modules/postgres v0.37.0
-	go.opentelemetry.io/otel v1.36.0
-	go.opentelemetry.io/otel/metric v1.36.0
-	go.opentelemetry.io/otel/trace v1.36.0
+	go.opentelemetry.io/otel v1.39.0
+	go.opentelemetry.io/otel/metric v1.39.0
+	go.opentelemetry.io/otel/trace v1.39.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.0
 )
 
 require (
 	github.com/a-h/parse v0.0.0-20250122154542-74294addb73e // indirect
-	github.com/a-h/templ v0.3.920 // indirect
-	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cli/browser v1.3.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/natefinch/atomic v1.0.1 // indirect
-	golang.org/x/mod v0.24.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	golang.org/x/mod v0.31.0 // indirect
+	golang.org/x/tools v0.40.0 // indirect
 )
 
 require (
@@ -41,18 +43,17 @@ require (
 	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/cpuguy83/dockercfg v0.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/deckarep/golang-set/v2 v2.8.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.8.0
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/dlclark/regexp2 v1.11.5 // indirect
 	github.com/docker/docker v28.0.1+incompatible // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/ebitengine/purego v0.8.2 // indirect
-	github.com/etnz/permute v1.0.2 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-faster/yaml v0.4.6 // indirect
-	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -63,10 +64,10 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/compress v1.18.1 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
@@ -80,23 +81,23 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/samber/lo v1.51.0 // indirect
-	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/samber/lo v1.51.0
+	github.com/segmentio/asm v1.2.1 // indirect
 	github.com/shirou/gopsutil/v4 v4.25.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
-	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/exp v0.0.0-20251219203646-944ab1f22d93 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
 	golang.org/x/time v0.12.0
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
--- challenge/internal/services/ngrok_test.go
+++ challenge/internal/services/ngrok_test.go
@@ -3,77 +3,84 @@ package services_test
 import (
 	"generate_technical_challenge_2025/internal/services"
 	"generate_technical_challenge_2025/internal/utils"
-	// "slices"
+	"slices"
+
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	NGROK_UUID = uuid.New()
+	NGROK_RNG  = utils.CreateRNGFromHash(NGROK_UUID)
+)
+
 func TestGenerateNgrokAliens(t *testing.T) {
-	firstRNG := utils.CreateRNGFromHash(UUID)
-	aliens := services.GenerateNgrokAliens(firstRNG, UUID)
+	firstRNG := utils.CreateRNGFromHash(NGROK_UUID)
+	aliens := services.GenerateNgrokAliens(firstRNG, NGROK_UUID)
 	assert.True(t, len(aliens) <= services.NUM_NGROK_ALIENS_UPPER_BOUND)
 	assert.True(t, len(aliens) >= services.NUM_NGROK_ALIENS_LOWER_BOUND)
 
 	// Aliens generated from the same UUID should be the same in length and content.
-	duplicateRng := utils.CreateRNGFromHash(UUID)
-	aliensAgain := services.GenerateNgrokAliens(duplicateRng, UUID)
+	duplicateRng := utils.CreateRNGFromHash(NGROK_UUID)
+	aliensAgain := services.GenerateNgrokAliens(duplicateRng, NGROK_UUID)
 	assert.Equal(t, aliens, aliensAgain)
 }
 
 func TestGenerateRandomFilterTests(t *testing.T) {
-	aliens := services.GenerateNgrokAliens(RNG, UUID)
+	aliens := services.GenerateNgrokAliens(NGROK_RNG, NGROK_UUID)
 
-	requests := services.GenerateRandomFilterTests(RNG, aliens)
+	requests := services.GenerateRandomFilterTests(NGROK_RNG, aliens)
 	assert.Len(t, requests, 5)
 }
 
-// func TestCalculateAlienDistance(t *testing.T) {
-// 	firstRNG := utils.CreateRNGFromHash(UUID)
-// 	aliens := services.GenerateNgrokAliens(firstRNG, UUID)
-// 	// Aliens are 0 distance from themselves.
-// 	dist := services.CalculateAlienDistance(aliens, aliens)
-// 	assert.True(t, dist == 0)
-//
-// 	distWithEmpty := services.CalculateAlienDistance(aliens, []services.DetailedAlien{})
-// 	assert.True(t, distWithEmpty == len(aliens))
-//
-// 	distWithOneMissing := services.CalculateAlienDistance(aliens, aliens[:len(aliens)-1])
-// 	assert.True(t, distWithOneMissing == 1)
-//
-// 	// Add an extra element.
-// 	extraAlien := services.GenerateDetailedAlien(firstRNG, UUID, 3)
-// 	longer := make([]services.DetailedAlien, 0, len(aliens)+1)
-// 	longer = append(longer, extraAlien)
-// 	longer = append(longer, aliens...)
-// 	distWithOneAdded := services.CalculateAlienDistance(aliens, longer)
-// 	assert.True(t, distWithOneAdded == 1)
-//
-// 	// Change the first element to have a different SPD value.
-// 	aliensToBeModified := slices.Clone(aliens)
-// 	tmp := aliensToBeModified[0]
-// 	tmp.Spd = min(1, services.ALIEN_ATK_HP_SPD_UPPER%(tmp.Spd+1))
-// 	aliensToBeModified[0] = tmp
-//
-// 	distWithOneChange := services.CalculateAlienDistance(aliens, aliensToBeModified)
-// 	assert.True(t, distWithOneChange == 1)
-//
-// 	// With two changes to a single alien, don't double-count it.
-// 	tmp.BaseAlien.Atk = min(1, services.ALIEN_ATK_HP_SPD_UPPER%(tmp.BaseAlien.Atk+1))
-// 	aliensToBeModified[0] = tmp
-// 	distWithTwoChanges := services.CalculateAlienDistance(aliens, aliensToBeModified)
-// 	assert.True(t, distWithTwoChanges == 1)
-//
-// 	// Changing the ID on an alien:
-// 	tmp.ID = tmp.ID + "1" // Guaranteed to change the ID--not a realistic ID but definitely different.
-// 	aliensToBeModified[0] = tmp
-// 	distWithIDChange := services.CalculateAlienDistance(aliens, aliensToBeModified)
-// 	assert.True(t, distWithIDChange == 1)
-//
-// 	allTheSameAlien := make([]services.DetailedAlien, len(aliens))
-// 	for idx := range aliens {
-// 		allTheSameAlien[idx] = aliens[0]
-// 	}
-// 	distWithAllTheSameAlien := services.CalculateAlienDistance(aliens, allTheSameAlien)
-// 	assert.True(t, distWithAllTheSameAlien == len(aliens)-1)
-// }
+func TestCalculateAlienDistance(t *testing.T) {
+	firstRNG := utils.CreateRNGFromHash(NGROK_UUID)
+	aliens := services.GenerateNgrokAliens(firstRNG, NGROK_UUID)
+	// Aliens are 0 distance from themselves.
+	dist := services.CalculateAlienDistance(aliens, aliens)
+	assert.True(t, dist == 0)
+
+	distWithEmpty := services.CalculateAlienDistance(aliens, []services.DetailedAlien{})
+	assert.True(t, distWithEmpty == len(aliens))
+
+	distWithOneMissing := services.CalculateAlienDistance(aliens, aliens[:len(aliens)-1])
+	assert.True(t, distWithOneMissing == 1)
+
+	// Add an extra element.
+	extraAlien := services.GenerateDetailedAlien(firstRNG, NGROK_UUID, 3)
+	longer := make([]services.DetailedAlien, 0, len(aliens)+1)
+	longer = append(longer, extraAlien)
+	longer = append(longer, aliens...)
+	distWithOneAdded := services.CalculateAlienDistance(aliens, longer)
+	assert.True(t, distWithOneAdded == 1)
+
+	// Change the first element to have a different SPD value.
+	aliensToBeModified := slices.Clone(aliens)
+	tmp := aliensToBeModified[0]
+	tmp.Spd = min(1, services.ALIEN_ATK_HP_SPD_UPPER%(tmp.Spd+1))
+	aliensToBeModified[0] = tmp
+
+	distWithOneChange := services.CalculateAlienDistance(aliens, aliensToBeModified)
+	assert.True(t, distWithOneChange == 1)
+
+	// With two changes to a single alien, don't double-count it.
+	tmp.BaseAlien.Atk = min(1, services.ALIEN_ATK_HP_SPD_UPPER%(tmp.BaseAlien.Atk+1))
+	aliensToBeModified[0] = tmp
+	distWithTwoChanges := services.CalculateAlienDistance(aliens, aliensToBeModified)
+	assert.True(t, distWithTwoChanges == 1)
+
+	// Changing the ID on an alien:
+	tmp.ID = tmp.ID + "1" // Guaranteed to change the ID--not a realistic ID but definitely different.
+	aliensToBeModified[0] = tmp
+	distWithIDChange := services.CalculateAlienDistance(aliens, aliensToBeModified)
+	assert.True(t, distWithIDChange == 1)
+
+	allTheSameAlien := make([]services.DetailedAlien, len(aliens))
+	for idx := range aliens {
+		allTheSameAlien[idx] = aliens[0]
+	}
+	distWithAllTheSameAlien := services.CalculateAlienDistance(aliens, allTheSameAlien)
+	assert.True(t, distWithAllTheSameAlien == len(aliens)-1)
+}
